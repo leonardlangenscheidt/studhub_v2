223ad495d9456876518ae7fb2f83e4db2fac0f18
Leonard Langenscheidt <leonardlangenscheidt@gmail.com>
2013-08-01 11:28:18 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Studhub::Application.config.secret_key_base = '4724b9e8ee9f935e107ff1121c32fec742361228f0db3368fadea0e7efcb89494d727a55e6b91bfa11b47bd141978d15e240655cfdf662e2ce007a0c523889da'
