e92bcf402669405fe11887bcc2e35ce4aaaef077
Leonard Langenscheidt <leonardlangenscheidt@gmail.com>
2013-08-04 15:41:53 -0400

    adding seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+puts "Adding a bunch of Earrings"
+
+Earring.create(vendor:"Tiffany & Co.", collection: "Else Peretti", design: "Full Heart", material: "Silver", size: 11, price: 150, sku: "TC1635936", inventory: 2, description: "Earrings in silver, crazy awesome.")
+Earring.create(vendor:"Tiffany & Co.", collection: "Else Peretti", design: "Open Heart", material: "18k Gold", size: 12, price: 350, sku: "TC12318936", inventory: 4, description: "Earrings in 18k gold, for pierced ears.")
+Earring.create(vendor:"Tiffany & Co.", collection: "Else Peretti", design: "Bean", material: "Rose Gold", size: 8, price: 80, sku: "TC45354356", inventory: 3, description: "One solid earring. Awesome.")
+Earring.create(vendor:"Tiffany & Co.", collection: "Else Peretti", design: "Full Heart", material: "Silver", size: 11, price: 150, sku: "TC1635936", inventory: 2, description: "Earrings in silver, crazy awesome.")
+Earring.create(vendor:"Tiffany & Co.", collection: "Else Peretti", design: "Open Heart", material: "18k Gold", size: 12, price: 350, sku: "TC12318936", inventory: 4, description: "Earrings in 18k gold, for pierced ears.")
+Earring.create(vendor:"Tiffany & Co.", collection: "Else Peretti", design: "Bean", material: "Rose Gold", size: 8, price: 80, sku: "TC45354356", inventory: 3, description: "One solid earring. Awesome.")
+Earring.create(vendor:"Tiffany & Co.", collection: "Else Peretti", design: "Full Heart", material: "Silver", size: 11, price: 150, sku: "TC1635936", inventory: 2, description: "Earrings in silver, crazy awesome.")
+Earring.create(vendor:"Tiffany & Co.", collection: "Else Peretti", design: "Open Heart", material: "18k Gold", size: 12, price: 350, sku: "TC12318936", inventory: 4, description: "Earrings in 18k gold, for pierced ears.")
+Earring.create(vendor:"Tiffany & Co.", collection: "Else Peretti", design: "Bean", material: "Rose Gold", size: 8, price: 80, sku: "TC45354356", inventory: 3, description: "One solid earring. Awesome.")
+Earring.create(vendor:"Tiffany & Co.", collection: "Else Peretti", design: "Full Heart", material: "Silver", size: 11, price: 150, sku: "TC1635936", inventory: 2, description: "Earrings in silver, crazy awesome.")
+Earring.create(vendor:"Tiffany & Co.", collection: "Else Peretti", design: "Open Heart", material: "18k Gold", size: 12, price: 350, sku: "TC12318936", inventory: 4, description: "Earrings in 18k gold, for pierced ears.")
+Earring.create(vendor:"Tiffany & Co.", collection: "Else Peretti", design: "Bean", material: "Rose Gold", size: 8, price: 80, sku: "TC45354356", inventory: 3, description: "One solid earring. Awesome.")
+
+puts "Done adding Earrings"
