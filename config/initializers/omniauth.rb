a8a4c4e4d9a8b4acd34b1c1548ee6c5ac5639d57
Leonard Langenscheidt <leonardlangenscheidt@gmail.com>
2013-08-04 13:09:37 -0400

    changed some syntax in omniauth.rb. WORKS

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,5 @@
+# require 'constants.rb'
+
 Rails.application.config.middleware.use OmniAuth::Builder do
-	provider :facebook, FACEBOOK_KEY, FACEBOOK_SECRET :display => 'popup'
+	provider :facebook, FACEBOOK_KEY, FACEBOOK_SECRET, :display => 'popup'
 end
