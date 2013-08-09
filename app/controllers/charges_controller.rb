01101b5c27c14334d72d7f895b777a39ee462026
Leonard Langenscheidt <leonardlangenscheidt@gmail.com>
2013-08-09 10:44:57 -0400

    added cc errors, some spacing, redirect_to's

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -8,7 +8,7 @@ class ChargesController < ApplicationController
 	  	@user = current_user
 
 	  	customer = Stripe::Customer.create(
-	    	:email => @user.email,
+	    	:email => 'user@example.com',
 	    	:card  => params[:stripeToken]
 	  	)
 
@@ -19,8 +19,16 @@ class ChargesController < ApplicationController
 	    	:currency    => 'usd'
 	  	)
 
-		rescue Stripe::CardError => e
-	  	flash[:error] = e.message
-	  	redirect_to charges_path
+	  	if charge
+	  		flash[:notice] = "Thank you! You just purchased the #{@earring.material} #{@earring.design} earring from the #{@earring.vendor} #{@earring.collection} collection for $#{@amount}.00!"
+	  		redirect_to earrings_path
+	  	else
+	  		flash[:notice] = Stripe::CardError.message
+	  		redirect_to earring_path(@earring)
+	  	end
+
+		# rescue Stripe::CardError => e
+	 #  	flash[:error] = e.message
+	 #  	redirect_to charges_path
 	end
 end
