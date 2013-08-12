a847c1f17a7e26acc4077a78caac019b1f3a116b
Leonard Langenscheidt <leonardlangenscheidt@gmail.com>
2013-08-12 10:06:28 -0400

    change picture syntex - text2

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,6 @@
 # require 'constants.rb'
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-	provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'], :display => 'popup'
+	provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'], :display => 'popup', :image_size => 'normal'
 	# provider :facebook, FACEBOOK_KEY, FACEBOOK_SECRET, :display => 'popup'
 end
