cfaac42319d4abdb269c14122aefe0f7e844386d
Leonard Langenscheidt <leonardlangenscheidt@gmail.com>
2013-08-14 13:53:22 -0400

    syntax in routes

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,7 @@ Studhub::Application.routes.draw do
 		resource :charges
 	end
 
-	match 'earrings/:earring_id/:buyorsell/users/:user_id' => 'users#edit', :via =>[:get, :patch]
+	match 'earrings/:earring_id/:buyorsell/users/:user_id' => 'users#edit', :via =>[:get, :patch, :post]
 	get 'earrings/:earring_id/:buyorsell/summary' => 'earrings#summary'
 
 
