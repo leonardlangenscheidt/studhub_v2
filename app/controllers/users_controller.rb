e1b8a3df57ccd6ece85e9ee7e2bb1d0ff61ea834
Leonard Langenscheidt <leonardlangenscheidt@gmail.com>
2013-08-15 14:51:04 -0400

    moar tries

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,9 +2,6 @@ class UsersController < ApplicationController
 	def show
 		@user = User.find(params[:id])
 		@orders = @user.orders
-		unless @user.addresses == nil
-			@addresses = @user.addresses
-		end
 	end
 	def index
 	end
