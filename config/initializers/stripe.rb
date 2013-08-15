047e1354ed6cb5057d6bed0b44b145b1c38395a7
Leonard Langenscheidt <leonardlangenscheidt@gmail.com>
2013-08-15 14:35:44 -0400

    production version

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,8 +1,8 @@
 Rails.configuration.stripe = {
-  # :publishable_key => ENV['PUBLISHABLE_KEY'],
-  # :secret_key      => ENV['SECRET_KEY']
-  :publishable_key => PUBLISHABLE_KEY,
-  :secret_key => SECRET_KEY
+  :publishable_key => ENV['PUBLISHABLE_KEY'],
+  :secret_key      => ENV['SECRET_KEY']
+  # :publishable_key => PUBLISHABLE_KEY,
+  # :secret_key => SECRET_KEY
 }
 
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
