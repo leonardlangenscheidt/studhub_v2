59c69a6f9ed769017c351e7ca3cf5ce898640184
Leonard Langenscheidt <leonardlangenscheidt@gmail.com>
2013-10-17 10:35:05 -0400

    profile routing and dwolla reestablish

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,6 +3,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
 	provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'], :display => 'popup', :image_size => 'normal'
 	# provider :facebook, FACEBOOK_KEY, FACEBOOK_SECRET, :display => 'popup'
-	provider :dwolla, ENV['DWOLLA_KEY'], ENV['DWOLLA_SECRET'], :scope => 'accountinfofull|send|request', :display => 'popup', :image_size => 'normal',
-	# :provider_ignores_state => true
+	provider :dwolla, ENV['DWOLLA_KEY'], ENV['DWOLLA_SECRET'], :scope => 'accountinfofull|send|request', :display => 'popup', :image_size => 'normal', :provider_ignores_state => true
 end
