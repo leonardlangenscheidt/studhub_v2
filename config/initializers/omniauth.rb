69db818f6d7846794c6e0288b0f821b7436c7cd9
Leonard Langenscheidt <leonardlangenscheidt@gmail.com>
2013-10-22 11:57:39 -0400

    modified omniauth.rb

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,9 +1,9 @@
 # require 'constants.rb'
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-	# provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'], :display => 'popup', :image_size => 'normal'
-	# provider :paypal, ENV['PAYPAL_KEY'], ENV['PAYPAL_SECRET'], sandbox: true, scope: "openid"
-	provider :facebook, FACEBOOK_KEY, FACEBOOK_SECRET, :display => 'popup'
-	provider :paypal, PAYPAL_KEY, PAYPAL_SECRET, sandbox: true, scope: "openid"
+	provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'], :display => 'popup', :image_size => 'normal'
+	provider :paypal, ENV['PAYPAL_KEY'], ENV['PAYPAL_SECRET'], sandbox: true, scope: "openid"
+	# provider :facebook, FACEBOOK_KEY, FACEBOOK_SECRET, :display => 'popup'
+	# provider :paypal, PAYPAL_KEY, PAYPAL_SECRET, sandbox: true, scope: "openid"
 	provider :dwolla, ENV['DWOLLA_KEY'], ENV['DWOLLA_SECRET'], :scope => 'accountinfofull|send|request', :display => 'popup', :image_size => 'normal', :provider_ignores_state => true
 end
