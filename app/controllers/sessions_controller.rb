722755a104e1c2ee88e167d87a3d5d6f4270002e
Leonard Langenscheidt <leonardlangenscheidt@gmail.com>
2013-10-28 14:00:23 -0400

    new user creation

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -13,31 +13,32 @@ class SessionsController < ApplicationController
 
 		if signed_in?
 			if @identity.user == current_user
-			# User is signed in so they are trying to link an identity with their
-			# account. But we found the identity and the user associated with it
-			# is the current user. So the identity is already associated with
-			# this user. So let's display an error message.
-			flash[:notice] = "Already linked that account!"
-			redirect_to root_url
+				# User is signed in so they are trying to link an identity with their
+				# account. But we found the identity and the user associated with it
+				# is the current user. So the identity is already associated with
+				# this user. So let's display an error message.
+				flash[:notice] = "Already linked that account!"
+				redirect_to root_url
 			else
-			# The identity is not associated with the current_user so lets
-			# associate the identity
-			@identity.user = current_user
-			@identity.save()
-			flash[:notice] = "Successfully linked that account!"
-			redirect_to root_url
+				# The identity is not associated with the current_user so lets
+				# associate the identity
+				@identity.user = current_user
+				@identity.save()
+				flash[:notice] = "Successfully linked that account!"
+				redirect_to root_url
 			end
 		  else
 			if @identity.user.present?
-			# The identity we found had a user associated with it so let's
-			# just log them in here
-			self.current_user = @identity.user
-			flash[:notice] = "Signed in!"
-			redirect_to root_url
-			# else
-			# # No user associated with the identity so we need to create a new one
-			# flash[:notice] = "Please finish registering"
-			# redirect_to new_user_url
+				# The identity we found had a user associated with it so let's
+				# just log them in here
+				self.current_user = @identity.user
+				flash[:notice] = "Signed in!"
+				redirect_to root_url
+			else
+				# No user associated with the identity so we need to create a new one
+				@user = User.create_with_omniauth(auth)
+				flash[:notice] = "Signed up!"
+				redirect_to root_url
 			end
 		  end
 
