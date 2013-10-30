e75b1b3d6e267f029cd0a175b8fef196f5a5425b
Leonard Langenscheidt <leonardlangenscheidt@gmail.com>
2013-10-30 09:20:15 -0400

    further errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,15 +9,16 @@ class ApplicationController < ActionController::Base
 		@current_user ||= User.find_by_id(session[:user_id]) if session[:user_id]
   	end
 
-  	def signed_in?
-		!!current_user
-  	end
-  	helper_method :current_user, :signed_in?
+  # 	def signed_in?
+		# !!current_user
+  # 	end
+  	helper_method :current_user
+  	# , :signed_in?
 
-  	def current_user=(user)
-		@current_user = user
-		session[:user_id] = user.nil? ? user : user.id
-  	end
+  # 	def current_user=(user)
+		# @current_user = user
+		# session[:user_id] = user.nil? ? user : user.id
+  # 	end
 
 	# def current_user
  #  		@current_user ||= User.find(session[:user_id]) if session[:user_id]
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,17 +3,17 @@ class User < ActiveRecord::Base
   	has_many :orders
 	has_many :addresses
 
-  	# def self.create_with_omniauth(info)
-   #  		create(name: info['name'], email: info['email'])
-  	# end
+  	def self.create_with_omniauth(auth)
+    		create(name: auth.info.name, email: auth.info.email)
+  	end
 
-	def self.create_with_omniauth(auth)
-		where(auth).first_or_initialize.tap do |user|
-			user.name = auth.info.name
-			user.email = auth.info.email
-			user.save!
-		end
-	end
+	# def self.create_with_omniauth(auth)
+	# 	where(auth).first_or_initialize.tap do |user|
+	# 		user.name = auth.info.name
+	# 		user.email = auth.info.email
+	# 		user.save!
+	# 	end
+	# end
 
 	# def self.find_with_omniauth(auth)
 	# 	find_by_provider_and_uid(auth['provider'], auth['uid'])
