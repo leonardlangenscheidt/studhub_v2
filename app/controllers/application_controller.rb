9c2ac8fed2b148b8221a7140519fb78e7b8d2ac8
Leonard Langenscheidt <leonardlangenscheidt@gmail.com>
2013-10-30 09:47:43 -0400

    current_user= method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,15 +14,17 @@ class ApplicationController < ActionController::Base
  #  	end
  #  	helper_method :current_user, :signed_in?
 
- #  	def current_user=(user)
-	# 	@current_user = user
-	# 	session[:user_id] = user.nil? ? user : user.id
- #  	end
+
 
 	def current_user
   		@current_user ||= User.find(session[:user_id]) if session[:user_id]
 	end
 
 	helper_method :current_user
+
+	  def current_user=(user)
+		@current_user = user
+		session[:user_id] = user.nil? ? user : user.id
+  	end
 
 end
