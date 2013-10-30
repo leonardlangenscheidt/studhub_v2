2febf1a212c2bcb7dde087c3e8fd142b7106fd8a
Leonard Langenscheidt <leonardlangenscheidt@gmail.com>
2013-10-30 09:15:41 -0400

    user.provider error..

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,14 +8,9 @@ class User < ActiveRecord::Base
   	# end
 
 	def self.create_with_omniauth(auth)
-		# where(auth.slice(:provider, :uid)).first_or_initialize.tap do |user|
 		where(auth).first_or_initialize.tap do |user|
-			# user.provider = auth.provider
-			# user.uid = auth.uid
 			user.name = auth.info.name
 			user.email = auth.info.email
-			# user.oauth_token = auth.credentials.token
-			# user.oauth_expires_at = Time.at(auth.credentials.expires_at)
 			user.save!
 		end
 	end
