030afbad5251a001bcdc5040abe4de3370c51256
Leonard Langenscheidt <leonardlangenscheidt@gmail.com>
2013-10-30 09:01:52 -0400

    little current_user change

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,8 @@ class User < ActiveRecord::Base
   	# end
 
 	def self.create_with_omniauth(auth)
-		where(auth.slice(:provider, :uid)).first_or_initialize.tap do |user|
+		# where(auth.slice(:provider, :uid)).first_or_initialize.tap do |user|
+		where(auth).first_or_initialize.tap do |user|
 			# user.provider = auth.provider
 			# user.uid = auth.uid
 			user.name = auth.info.name
