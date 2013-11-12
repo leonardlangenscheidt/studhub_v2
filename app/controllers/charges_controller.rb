22676a7c23f3157a1dc4da18184fe0a4dde58381
Leonard Langenscheidt <leonardlangenscheidt@gmail.com>
2013-11-12 11:43:41 -0500

    order creation mechanism

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -22,24 +22,25 @@ class ChargesController < ApplicationController
 	  	)
 
 	  	if charge
-	  		@order = Order.create(
-			:user_id => @user.id,
-			:earring_id => @earring.id,
-			:price_paid => @earring.price,
-			:status => "Confirmed",
-			:address_id => @address.id,
-			:buy => @buy
-			)
-			@earring.inventory = @earring.inventory - 1
-			@earring.save
-			UserMailer.purchase_email(@order).deliver
-	  		flash[:notice] = %Q[Thank you! You just purchased the #{@earring.material} #{@earring.design} earring from the #{@earring.vendor} #{@earring.collection} collection for $#{@amount}.00! <a href="#" onclick="
-			window.open(
-			  'http://www.facebook.com/sharer/sharer.php?s=100&p[url]=http://agile-peak-7882.herokuapp.com/&p[images][0]=&p[title]=Stud%20and%20Found&p[summary]=I%20just%20bought%20a%20match%20for%20the%20single%20earring%20I%20had%20left%20over.%20Woohoo!',
-			  'facebook-share-dialog',
-			  'width=626,height=436');
-			return false;">Share on Facebook</a>].html_safe
-	  		redirect_to "/user/orders/#{@order.id}"
+	  		redirect_to user_orders_path(:earring_id =>@earring.id, :address_id =>@address.id, :buy => @buy), :method => :post
+	  # 		@order = Order.create(
+			# :user_id => @user.id,
+			# :earring_id => @earring.id,
+			# :price_paid => @earring.price,
+			# :status => "Confirmed",
+			# :address_id => @address.id,
+			# :buy => @buy
+			# )
+			# @earring.inventory = @earring.inventory - 1
+			# @earring.save
+			# UserMailer.purchase_email(@order).deliver
+	  # 		flash[:notice] = %Q[Thank you! You just purchased the #{@earring.material} #{@earring.design} earring from the #{@earring.vendor} #{@earring.collection} collection for $#{@amount}.00! <a href="#" onclick="
+			# window.open(
+			#   'http://www.facebook.com/sharer/sharer.php?s=100&p[url]=http://agile-peak-7882.herokuapp.com/&p[images][0]=&p[title]=Stud%20and%20Found&p[summary]=I%20just%20bought%20a%20match%20for%20the%20single%20earring%20I%20had%20left%20over.%20Woohoo!',
+			#   'facebook-share-dialog',
+			#   'width=626,height=436');
+			# return false;">Share on Facebook</a>].html_safe
+	  # 		redirect_to "/user/orders/#{@order.id}"
 	  	else
 	  		flash[:notice] = Stripe::CardError.message
 	  		redirect_to earring_path(@earring)
--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -50,20 +50,36 @@ class OrdersController < ApplicationController
 		:user_id => @user.id,
 		:earring_id => @earring.id,
 		:price_paid => @earring.price,
-		:status => "Awaiting Shipment",
+		if @buy == true
+			:status => "Confirmed"
+		else
+			:status => "Awaiting Shipment",
+		end
 		:address_id => @address.id,
 		:buy => @buy
 		)
 		if @order.save
-			@earring.inventory = @earring.inventory + 1
-			@earring.save
-			UserMailer.sell_email(@order).deliver
-			flash[:notice] = %Q[Thank you! You just sold the #{@earring.material} #{@earring.design} earring from the #{@earring.vendor} #{@earring.collection} collection for $#{@earring.price}.00! <a href="#" onclick="
-			window.open(
-			  'http://www.facebook.com/sharer/sharer.php?s=100&p[url]=http://agile-peak-7882.herokuapp.com/&p[images][0]=&p[title]=Stud%20and%20Found&p[summary]=I%20just%20sold%20a%20single%20earring%20I%20had%20left%20over.%20Woohoo!',
-			  'facebook-share-dialog',
-			  'width=626,height=436');
-			return false;">Share on Facebook</a>].html_safe
+			if @order.buy == true
+				@earring.inventory = @earring.inventory - 1
+				@earring.save
+				UserMailer.purchase_email(@order).deliver
+		  		flash[:notice] = %Q[Thank you! You just purchased the #{@earring.material} #{@earring.design} earring from the #{@earring.vendor} #{@earring.collection} collection for $#{@amount}.00! <a href="#" onclick="
+				window.open(
+				  'http://www.facebook.com/sharer/sharer.php?s=100&p[url]=http://agile-peak-7882.herokuapp.com/&p[images][0]=&p[title]=Stud%20and%20Found&p[summary]=I%20just%20bought%20a%20match%20for%20the%20single%20earring%20I%20had%20left%20over.%20Woohoo!',
+				  'facebook-share-dialog',
+				  'width=626,height=436');
+				return false;">Share on Facebook</a>].html_safe
+			else
+				@earring.inventory = @earring.inventory + 1
+				@earring.save
+				UserMailer.sell_email(@order).deliver
+				flash[:notice] = %Q[Thank you! You just sold the #{@earring.material} #{@earring.design} earring from the #{@earring.vendor} #{@earring.collection} collection for $#{@earring.price}.00! <a href="#" onclick="
+				window.open(
+				  'http://www.facebook.com/sharer/sharer.php?s=100&p[url]=http://agile-peak-7882.herokuapp.com/&p[images][0]=&p[title]=Stud%20and%20Found&p[summary]=I%20just%20sold%20a%20single%20earring%20I%20had%20left%20over.%20Woohoo!',
+				  'facebook-share-dialog',
+				  'width=626,height=436');
+				return false;">Share on Facebook</a>].html_safe
+			end
 			redirect_to "/user/orders/#{@order.id}"
 		else
 			flash[:notice] = "Something went wrong. Please try again."
