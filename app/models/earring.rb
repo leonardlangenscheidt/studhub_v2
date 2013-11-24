ba12785604a2134ece33fa62e81112532a7ca4b0
Leonard Langenscheidt <leonardlangenscheidt@gmail.com>
2013-11-24 11:15:49 -0500

    some styling

--- app/models/earring.rb
+++ app/models/earring.rb
@@ -1,5 +1,5 @@
 class Earring < ActiveRecord::Base
-	has_attached_file :image, styles: { medium: "320x240>", :small => "160x120>"}
+	has_attached_file :image, styles: { large: "640x480>", medium: "320x240>", :small => "160x120>"}
 	validates_attachment :image, presence: true
 	has_many :orders
 end
