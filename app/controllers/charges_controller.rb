791ed594aea9cb4957b5155134f29341491ff333
Leonard Langenscheidt <leonardlangenscheidt@gmail.com>
2013-11-26 09:52:01 -0500

    buys now direct not remote

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -5,7 +5,6 @@ class ChargesController < ApplicationController
 	def create
 	  	@earring = Earring.find(params[:earring_id])
 	  	@address = Address.find(params[:address_id])
-	  	@amount =@earring.price
 	  	@user = current_user
 
 	  	customer = Stripe::Customer.create(
@@ -15,13 +14,38 @@ class ChargesController < ApplicationController
 
 	  	charge = Stripe::Charge.create(
 	    	:customer    => customer.id,
-	    	:amount      => @amount,
+	    	:amount      => @earring.price,
 	    	:description => 'Rails Stripe customer',
 	    	:currency    => 'usd'
 	  	)
 
 	  	if charge
-	  		redirect_to "/remotecreate?earring_id=#{@earring.id}&address_id=#{@address.id}", :method => :post
+	  		@order = Order.create(
+			:user_id => @user.id,
+			:earring_id => @earring.id,
+			:price_paid => @earring.price,
+			:address_id => @address.id,
+			:tax => 0,
+			:number => 1,
+			:status => "Confirmed"
+			)
+			@order.save
+			if @order.address.used == true
+				@earring.used_inventory = @earring.used_inventory - 1
+			else
+				@earring.inventory = @earring.inventory - 1
+			end
+			@earring.save
+			@address.order_id = @order.id
+			@address.save
+			UserMailer.purchase_email(@order).deliver
+	  		flash[:notice] = %Q[Thank you! You just purchased the #{@earring.material} #{@earring.design} earring from the #{@earring.vendor} #{@earring.collection} collection for $#{@earring.price}.00! <a href="#" onclick="
+			window.open(
+			  'http://www.facebook.com/sharer/sharer.php?s=100&p[url]=http://studandfound.com/&p[images][0]=&p[title]=Stud%20and%20Found&p[summary]=I%20just%20bought%20a%20match%20for%20the%20single%20earring%20I%20had%20left%20over.%20Woohoo!',
+			  'facebook-share-dialog',
+			  'width=626,height=436');
+			return false;">Share on Facebook</a>].html_safe
+			redirect_to "/user/orders/#{@order.id}"
 	  	else
 	  		flash[:notice] = Stripe::CardError.message
 	  		redirect_to earring_path(@earring)
--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -67,53 +67,53 @@ class OrdersController < ApplicationController
 	end
 
 	def remotecreate
-	  	@earring = Earring.find(params[:earring_id])
-	  	@address = Address.find(params[:address_id])
-	  	@user = current_user
-		@order = Order.create(
-		:user_id => @user.id,
-		:earring_id => @earring.id,
-		:price_paid => @earring.price,
-		:address_id => @address.id,
-		:tax => 0,
-		:number => 1,
-		:status => "Confirmed"
-		)
-		if @order.save
-			if @order.address.buy == true
-				if @order.address.used == true
-					@earring.used_inventory = @earring.used_inventory - 1
-				else
-					@earring.inventory = @earring.inventory - 1
-				end
-				@earring.save
-				@address.order_id = @order.id
-				@address.save
-				UserMailer.purchase_email(@order).deliver
-		  		flash[:notice] = %Q[Thank you! You just purchased the #{@earring.material} #{@earring.design} earring from the #{@earring.vendor} #{@earring.collection} collection for $#{@earring.price}.00! <a href="#" onclick="
-				window.open(
-				  'http://www.facebook.com/sharer/sharer.php?s=100&p[url]=http://studandfound.com/&p[images][0]=&p[title]=Stud%20and%20Found&p[summary]=I%20just%20bought%20a%20match%20for%20the%20single%20earring%20I%20had%20left%20over.%20Woohoo!',
-				  'facebook-share-dialog',
-				  'width=626,height=436');
-				return false;">Share on Facebook</a>].html_safe
-			else
-				@earring.used_inventory = @earring.used_inventory + 1
-				@earring.save
-				@address.order_id = @order.id
-				@address.save
-				UserMailer.sell_email(@order).deliver
-				flash[:notice] = %Q[Thank you! You just sold the #{@earring.material} #{@earring.design} earring from the #{@earring.vendor} #{@earring.collection} collection for $#{@earring.price}.00! <a href="#" onclick="
-				window.open(
-				  'http://www.facebook.com/sharer/sharer.php?s=100&p[url]=http://studandfound.com/&p[images][0]=&p[title]=Stud%20and%20Found&p[summary]=I%20just%20sold%20a%20single%20earring%20I%20had%20left%20over.%20Woohoo!',
-				  'facebook-share-dialog',
-				  'width=626,height=436');
-				return false;">Share on Facebook</a>].html_safe
-			end
-			redirect_to "/user/orders/#{@order.id}"
-		else
-			flash[:notice] = "Something went wrong. Please try again."
-			redirect_to "/earrings/#{@earring.id}"
-		end
+	 #  	@earring = Earring.find(params[:earring_id])
+	 #  	@address = Address.find(params[:address_id])
+	 #  	@user = current_user
+		# @order = Order.create(
+		# :user_id => @user.id,
+		# :earring_id => @earring.id,
+		# :price_paid => @earring.price,
+		# :address_id => @address.id,
+		# :tax => 0,
+		# :number => 1,
+		# :status => "Confirmed"
+		# )
+		# if @order.save
+		# 	if @order.address.buy == true
+		# 		if @order.address.used == true
+		# 			@earring.used_inventory = @earring.used_inventory - 1
+		# 		else
+		# 			@earring.inventory = @earring.inventory - 1
+		# 		end
+		# 		@earring.save
+		# 		@address.order_id = @order.id
+		# 		@address.save
+		# 		UserMailer.purchase_email(@order).deliver
+		#   		flash[:notice] = %Q[Thank you! You just purchased the #{@earring.material} #{@earring.design} earring from the #{@earring.vendor} #{@earring.collection} collection for $#{@earring.price}.00! <a href="#" onclick="
+		# 		window.open(
+		# 		  'http://www.facebook.com/sharer/sharer.php?s=100&p[url]=http://studandfound.com/&p[images][0]=&p[title]=Stud%20and%20Found&p[summary]=I%20just%20bought%20a%20match%20for%20the%20single%20earring%20I%20had%20left%20over.%20Woohoo!',
+		# 		  'facebook-share-dialog',
+		# 		  'width=626,height=436');
+		# 		return false;">Share on Facebook</a>].html_safe
+		# 	else
+		# 		@earring.used_inventory = @earring.used_inventory + 1
+		# 		@earring.save
+		# 		@address.order_id = @order.id
+		# 		@address.save
+		# 		UserMailer.sell_email(@order).deliver
+		# 		flash[:notice] = %Q[Thank you! You just sold the #{@earring.material} #{@earring.design} earring from the #{@earring.vendor} #{@earring.collection} collection for $#{@earring.price}.00! <a href="#" onclick="
+		# 		window.open(
+		# 		  'http://www.facebook.com/sharer/sharer.php?s=100&p[url]=http://studandfound.com/&p[images][0]=&p[title]=Stud%20and%20Found&p[summary]=I%20just%20sold%20a%20single%20earring%20I%20had%20left%20over.%20Woohoo!',
+		# 		  'facebook-share-dialog',
+		# 		  'width=626,height=436');
+		# 		return false;">Share on Facebook</a>].html_safe
+		# 	end
+		# 	redirect_to "/user/orders/#{@order.id}"
+		# else
+		# 	flash[:notice] = "Something went wrong. Please try again."
+		# 	redirect_to "/earrings/#{@earring.id}"
+		# end
 	end
 
 	private
