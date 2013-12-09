031b11bda0386d59d52ef04aee4e0d9f26d17ff0
Leonard Langenscheidt <leonardlangenscheidt@gmail.com>
2013-12-09 14:59:05 -0500

    little changes

--- app/controllers/earrings_controller.rb
+++ app/controllers/earrings_controller.rb
@@ -80,7 +80,7 @@ class EarringsController < ApplicationController
 	def destroy
 		@earring.destroy
 		respond_to do |format|
-			format.html { redirect_to 'inv/inventory' }
+			format.html { redirect_to '/inv/inventory' }
 			format.json { head :no_content }
 		end
 	end
