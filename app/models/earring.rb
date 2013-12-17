4459ec76bb7d0df6378dffd99dc7829b86b089f4
Leonard Langenscheidt <leonardlangenscheidt@gmail.com>
2013-12-17 14:51:59 -0500

    data validation for earrings, separate shows for index, which-earring-side

--- app/models/earring.rb
+++ app/models/earring.rb
@@ -4,4 +4,5 @@ class Earring < ActiveRecord::Base
 	has_attached_file :imageright, styles: { large: "980x980>", medium: "480x480>", :small => "240x240>"}
 	validates_attachment :image, presence: true
 	has_many :orders
+	validates :vendor, :collection, :design, :material, :size, :price, :sku, :inventory, :used_inventory, :description, :sides, :easyRestock, presence: true
 end
